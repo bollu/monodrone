0a02dc5d283d88295720de77fd08088e8e2d5a54
Siddharth Bhat <siddu.druid@gmail.com>
2024-05-28 01:57:49 +0100

    chore: add midi playback

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 #![allow(rustdoc::missing_crate_level_docs)] // it's an example
 
+use std::{fs::File, sync::Arc};
+
 use midi::Message::Start;
 use raylib::prelude::*;
-
+use tinyaudio::{run_output_device, OutputDeviceParameters};
+use tinyaudio::prelude::*;
 
 const GOLDEN_RATIO: f32 = 1.61803398875;
 
+use rustysynth::{MidiFile, MidiFileSequencer, SoundFont, Synthesizer, SynthesizerSettings};
 use tracing_subscriber::layer::SubscriberExt;
 use tracing::{event, Level};
 mod monodroneffi;
@@ -48,11 +52,58 @@ fn main() {
     let track = monodroneffi::get_track(monodrone_ctx);
     event!(Level::INFO, "track: {:?}", track);
 
+    let sf2 = include_bytes!("../resources/TimGM6mb.sf2");
+    let mut sf2_cursor = std::io::Cursor::new(sf2);
+    // Load the SoundFont.
+    let sound_font = Arc::new(SoundFont::new(&mut sf2_cursor).unwrap());
+
+
+    let params = OutputDeviceParameters {
+        channels_count: 2,
+        sample_rate: 44100,
+        channel_sample_count: 4410,
+    };
+
+
+    // Create the MIDI file sequencer.
+    let settings = SynthesizerSettings::new(params.sample_rate as i32);
+    let mut synthesizer = Synthesizer::new(&sound_font, &settings).unwrap();
+    // Play some notes (middle C, E, G).
+    synthesizer.note_on(0, 60, 100);
+    synthesizer.note_on(0, 64, 100);
+    synthesizer.note_on(0, 67, 100);
+
+
+    // The output buffer (3 seconds).
+    let mut left: Vec<f32> = vec![0_f32; params.channel_sample_count];
+    let mut right: Vec<f32> = vec![0_f32; params.channel_sample_count];
+
+
+    // Start the audio output.
+    let _device = run_output_device(params, {
+        move |data| {
+            // Render the waveform.
+            synthesizer.render(&mut left[..], &mut right[..]);
+            for i in 0..data.len() {
+                if i % 2 == 0 {
+                    data[i] = left[i / 2];
+                } else {
+                    data[i] = right[i / 2];
+                }
+            }
+        }
+    })
+    .unwrap();
+
+    // Wait for 10 seconds.
+    std::thread::sleep(std::time::Duration::from_secs(3));
+
     event!(Level::INFO, "Starting up");
     let (mut rl, thread) = raylib::init()
         .size(640, 480)
         .title("Hello, World")
         .build();
+
 
     while !rl.window_should_close() {
         let track = monodroneffi::get_track(monodrone_ctx);
