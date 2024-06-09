aa1114f22d2ed1dc080408f92044db5f3652dc97
Siddharth Bhat <siddu.druid@gmail.com>
2024-06-09 12:07:31 +0100

    chore: build theory for modifying notes in a track while ensuring disjointedness of notes

--- Monodrone.lean
+++ Monodrone.lean
@@ -6,7 +6,9 @@ import Mathlib.Algebra.AddTorsor
 import Batteries.Data.RBMap
 import Mathlib.Data.List.Sort
 import Mathlib.Order.GaloisConnection
-open Lean
+import Lean.Elab.Tactic.Config
+import Monodrone.NoteOmega
+open Lean Meta
 open Batteries
 
 /-
@@ -27,6 +29,7 @@ def Pitch.lowerSemitone (p : Pitch) : Pitch := { pitch := p.pitch - 1 }
 def Pitch.raiseWhole (p : Pitch) : Pitch := { pitch := p.pitch + 2 }
 def Pitch.lowerWhole (p : Pitch) : Pitch := { pitch := p.pitch - 2 }
 
+def Note.lastNoteIx : Nat := 9999
 
 /-- A Note with a location. -/
 structure Note where
@@ -43,17 +46,63 @@ instance : Inhabited Note where
   default := { pitch := Pitch.middleC, start := 0, nsteps := 1, hnsteps := by decide }
 
 /-- The step when the note plays last. -/
-def Note.end (n : Note) : Nat := n.start + n.nsteps - 1
+@[note_omega]
+def Note.lastPlayed (n : Note) : Nat := n.start + n.nsteps - 1
 
-def Note.overlaps (n1 n2 : Note) : Prop := n1.start < n2.end && n2.start < n1.end
+def Note.overlaps (n1 n2 : Note) : Prop :=
+   n2.start ≤ n1.lastPlayed && n1.start ≤ n2.lastPlayed
 
-def Note.disjoint (n1 n2 : Note) : Prop := n1.end < n2.start || n2.end < n1.start
+instance : Decidable (Note.overlaps n1 n2) := by
+  simp [Note.overlaps]
+  infer_instance
+
+def Note.disjoint (n1 n2 : Note) : Prop :=
+  n1.lastPlayed < n2.start || n2.lastPlayed < n1.start
+
+instance : Decidable (Note.disjoint n1 n2) := by
+  simp [Note.disjoint]
+  infer_instance
+
+
+theorem Note.overlaps_if_not_disjoint (n1 n2 : Note) :
+    ¬ Note.disjoint n1 n2 → Note.overlaps n1 n2 := by
+  simp [Note.disjoint, Note.overlaps]
+
+theorem Note.disjoint_if_not_overlaps (n1 n2 : Note) :
+    ¬ Note.overlaps n1 n2 → Note.disjoint n1 n2 := by
+  simp [Note.disjoint, Note.overlaps]
+  intros h₁
+  omega
+
+theorem Note.disjoint_symm (n1 n2 : Note) :
+    Note.disjoint n1 n2 ↔ Note.disjoint n2 n1 := by
+  simp [Note.disjoint]
+  constructor <;> omega
+
+theorem Note.overlaps_symm (n1 n2 : Note) :
+    Note.overlaps n1 n2 ↔ Note.overlaps n2 n1 := by
+  simp [Note.overlaps]
+  constructor <;> omega
+
+def Note.contains (n : Note) (ix : Nat) : Prop :=
+  n.start ≤ ix && ix ≤ n.lastPlayed
+
+instance : Decidable (Note.contains n ix) := by
+  simp [Note.contains]
+  infer_instance
+
+def Note.containsNote (n1 n2 : Note) : Prop :=
+  n1.start ≤ n2.start && n2.lastPlayed ≤ n1.lastPlayed
+
+instance : Decidable (Note.containsNote n1 n2) := by
+  simp [Note.containsNote]
+  infer_instance
 
 def Note.compare (n1 n2 : Note) : Ordering :=
   if n1.start < n2.start then Ordering.lt
   else if n1.start = n2.start then
-    if n1.end < n2.end then Ordering.lt
-    else if n2.end < n1.end then Ordering.gt
+    if n1.lastPlayed < n2.lastPlayed then Ordering.lt
+    else if n2.lastPlayed < n1.lastPlayed then Ordering.gt
     else Ordering.eq
   else
     Ordering.gt
@@ -69,26 +118,113 @@ structure Track where
   notes : List Note
   /-- The notes are disjoint. -/
   hdisjoint : notes.Pairwise Note.disjoint
-  /-- The notes are sorted -/
-  hsorted : notes.Sorted (· ≤ ·)
   junk : Unit := ()  -- workaround for https://github.com/leanprover/lean4/issues/4278
-deriving DecidableEq, Repr
+deriving Repr, DecidableEq
 
-def Track.empty : Track := { notes := [], hdisjoint := by simp [], hsorted := by simp [] }
+/--
+Try to add a note to a track.
+Succeeds if there is space for the note. Fails otherwise,
+returning the same track. -/
+def Track.addNote (t : Track) (n : Note) : Track :=
+  if ht : t.notes.all (fun n' => decide (n.disjoint n')) then
+  { t with
+    notes := n :: t.notes
+    hdisjoint := by
+      simp_all only [List.all_eq_true,
+        decide_eq_true_eq, not_forall, Classical.not_imp]
+      simp [List.pairwise_cons, ht, t.hdisjoint]
+      assumption
+  }
+  else t
+
+open List in
+theorem List.pairwise_map' {l : List α} {f : α → β}
+    {hf : ∀ {a a' : α}, R a a' → S (f a) (f a')}
+    (hl : l.Pairwise R) : (l.map f).Pairwise S := by
+  induction l
+  case nil => simp
+  case cons a as ih =>
+    simp only [map, pairwise_cons, forall_mem_map_iff, *, hl]
+    constructor
+    · simp only [pairwise_cons] at hl
+      intros a ha
+      apply hf
+      apply hl.1
+      assumption
+    · simp only [pairwise_cons] at hl
+      exact (ih hl.2)
+
+theorem not_contains_of_disjoint_of_contains {n1 n2 : Note} {ix : Nat}
+    (hdisjoint : n1.disjoint n2) (hcontains : n1.contains ix) : ¬ n2.contains ix := by
+  simp [Note.disjoint, Note.contains] at *
+  intros h₂
+  have hn2 := n2.hnsteps
+  have hn1 := n1.hnsteps
+  simp_all [note_omega]
+  omega
+
+theorem disjoint_of_containsNote {nlarge nsmall m : Note}
+    (hcontains : nlarge.containsNote nsmall) (hdisjoint : nlarge.disjoint m) :
+    nsmall.disjoint m := by
+  simp [Note.disjoint, Note.containsNote] at *
+  have hnsmall := nsmall.hnsteps
+  have hnlarge := nlarge.hnsteps
+  simp_all [note_omega]
+  omega
+
+/-- Modify a note at the location 'loc' if it exists, as long as the
+modification does not increase the the length of the note. -/
+def Track.modifyNoteOfContains (t : Track) (loc : Nat) (f : Note → Note)
+  (hf : ∀ (n : Note), n.containsNote (f n)) : Track :=
+  { t with
+    notes := t.notes.map fun note =>
+      if note.contains loc then f note else note
+    hdisjoint := by
+      apply List.pairwise_map' (R := Note.disjoint) (S := Note.disjoint) t.hdisjoint
+      intros a a'
+      intros haa'
+      split_ifs
+      case pos ha ha' =>
+        have hcontra := not_contains_of_disjoint_of_contains haa' ha
+        contradiction
+      case neg ha ha' =>
+        apply disjoint_of_containsNote
+        apply hf
+        apply haa'
+      case pos ha ha' =>
+        rw [Note.disjoint_symm]
+        apply disjoint_of_containsNote
+        apply hf
+        rw [Note.disjoint_symm]
+        apply haa'
+      case neg ha ha' =>
+        exact haa'
+  }
+
+/--
+Try to remove note at location 'loc'.
+-/
+def Track.removeNoteAt (t : Track) (loc : Nat) : Track :=
+  { t with
+    notes := t.notes.filter (fun note => note.contains loc),
+    hdisjoint := List.Pairwise.filter _ t.hdisjoint
+  }
+
+def Track.empty : Track := { notes := [], hdisjoint := by simp [] }
 
 def Track.default : Track where
   notes := [ { pitch := Pitch.middleC, start := 0, nsteps := 1, hnsteps := by decide } ]
   hdisjoint := by simp [Note.disjoint]
-  hsorted := by simp [Note.compare]
 
 instance : Inhabited Track where
   default := Track.empty
 
-
 def Track.maxLength : Nat := 9999
 
 structure Cursor where
+  /-- How far the selection extends. -/
   a : Fin Track.maxLength
+   /-- Where the cursor is located. -/
   b : Fin Track.maxLength
 deriving DecidableEq, Repr, DecidableEq
 
@@ -120,12 +256,6 @@ def Cursor.moveUpOne (c : Cursor) : Cursor := (CursorMoveAction.up 1).act c
 
 def Cursor.moveDownHalfPage (c : Cursor) : Cursor := (CursorMoveAction.down 10).act c
 def Cursor.moveUpHalfPage (c : Cursor) : Cursor := (CursorMoveAction.up 10).act c
-
-inductive NoteAction
-| toggleNote
-| moveNoteUpSemitone
-| moveNoteDownSemitone
-deriving Inhabited, DecidableEq, Repr
 
 class Patchable (α : Type) (δ : Type) extends VAdd δ α where
   /-- Given the diff `f -δ→ g` and `f`, compute `g -(f @⁻¹ δ)→ f`.
@@ -180,27 +310,23 @@ instance : LawfulDiffable α (NaiveDiff α) where
   vadd_reverse_reverse := by simp [(· -ᵥ ·), (· +ᵥ ·), VAdd.vadd, Patchable.reverse]
   reverse_vsub := by simp [(· -ᵥ ·), (· +ᵥ ·), VAdd.vadd, Patchable.reverse]
 
-/-- A data structure which maintains the history of a given type. -/
+/--
+A data structure which maintains the history of a given type.
+past₂ ←p-  past₁ ←p- cur  -n→ c₁ -n→ c₂ ... -/
 structure HistoryStack (α : Type) (δ : Type) where
-  historyPrev : List δ  -- upon being applied, gives previous element.
+  /- upon being applied to `cur`, gives previous element. -/
+  historyPrev : List δ
   cur : α
-  historyNext: List δ -- upon being applied, gives next element
+   /- upon being applied, gives next element. -/
+  historyNext: List δ
 deriving Inhabited, Repr
 
-instance [DecidableEq α] : DecidableEq (HistoryStack α δ) := fun a b => by
-  -- rintros ⟨prev, cur, next⟩ := a
-  sorry
 
-/--  past₂ ←p  past₁ ←p cur  -n→ c₁ -n→ c₂ ... -/
 def HistoryStack.init {α : Type} (a : α) : HistoryStack α δ where
   historyPrev := []
   cur := a
   historyNext := []
 
-/--
-Given a state as follows:
- past₂ ←p-  past₁ ←p- cur  -n→ c₁ -n→ c₂ ... -/
---/
 def HistoryStack.prev (h : HistoryStack α δ) [Patchable α δ] : HistoryStack α δ :=
   match h.historyPrev with
   | [] => h
@@ -223,9 +349,7 @@ def HistoryStack.next (h : HistoryStack α δ) [Patchable α δ] : HistoryStack 
       historyNext := as
     }
 
-
 /-- Todo: show that prev / next are a galois connection. -/
-
 theorem HistoryStack.prev_next_eq_self_of_next_ne [Patchable α δ] [LawfulPatchable α δ]
     (h : HistoryStack α δ) (hprev : h.historyNext ≠ []) :
     (HistoryStack.prev (HistoryStack.next h)) = h := by
@@ -259,7 +383,7 @@ theorem HistoryStack.cur_prev_setForgettingFuture_eq_cur [DecidableEq α]
 
 /--  Undo followed by a redo will keep us at the current state. -/
 theorem HistoryStack.cur_next_prev_setForgettingFuture_eq_cur [DecidableEq α]
-    [Diffable α δ] [LawfulDiffable α δ]
+    [Diffable α δ] [LawfulPatchable α δ]
     (h : HistoryStack α δ) (newcur : α) (hnewcur : h.cur ≠ newcur) :
     (HistoryStack.setForgettingFuture newcur h).prev.next.cur = newcur := by
   simp [HistoryStack.setForgettingFuture, prev, next, hnewcur]
@@ -276,20 +400,31 @@ def HistoryStack.patchForgettingFuture [DecidableEq α] [Diffable α δ]
   historyPrev := (h.cur @⁻¹ p) :: h.historyPrev
   historyNext := []
 
+/--  Undo will take us back to the previous state. -/
+theorem HistoryStack.cur_prev_patchForgettingFuture_eq [DecidableEq α]
+    [Diffable α δ] [LawfulDiffable α δ]
+    (h : HistoryStack α δ) (patch : δ)  :
+    (HistoryStack.patchForgettingFuture h patch).prev.cur = h.cur := by
+  simp [HistoryStack.patchForgettingFuture, prev, next]
+
+
 structure RawContext where
-  track : Track
+  lastPlacedPitch : Pitch
+  track : HistoryStack Track (NaiveDiff Track)
   cursor : HistoryStack Cursor (NaiveDiff Cursor)
   junk : Unit := () -- Workaround for: 'https://github.com/leanprover/lean4/issues/4278'
-deriving Inhabited, DecidableEq
+deriving Inhabited
 
 def RawContext.empty : RawContext := {
-    track := Track.empty,
+    track := HistoryStack.init Track.empty,
     cursor := HistoryStack.init .atbegin,
+    lastPlacedPitch := Pitch.middleC
 }
 
 def RawContext.default : RawContext := {
-    track := Track.default,
+    track := HistoryStack.init Track.default,
     cursor := HistoryStack.init .atbegin,
+    lastPlacedPitch := Pitch.middleC
 }
 
 namespace ffi
@@ -310,11 +445,11 @@ We follow [json-c](https://json-c.github.io/json-c/json-c-0.17/doc/html/json__ob
 def newContext (_ : Unit) : RawContext := RawContext.default
 
 @[export monodrone_track_length]
-def trackLength (ctx : @&RawContext) : UInt64 := ctx.track.notes.length.toUInt64
+def trackLength (ctx : @&RawContext) : UInt64 := ctx.track.cur.notes.length.toUInt64
 
 @[export monodrone_track_get_note]
 def trackGetNote (ctx : @&RawContext) (ix : UInt64) : Note :=
-  ctx.track.notes.get! ix.toNat
+  ctx.track.cur.notes.get! ix.toNat
 
 @[export monodrone_note_get_pitch]
 def noteGetPitch (n : Note) : UInt64 := n.pitch.pitch
@@ -345,14 +480,38 @@ def RawContext.moveDownHalfPage (ctx : RawContext) : RawContext :=
 def RawContext.moveUpHalfPage (ctx : RawContext) : RawContext :=
   { ctx with cursor := ctx.cursor.modifyForgettingFuture Cursor.moveUpHalfPage }
 
-def RawContext.toggleNote (ctx : RawContext) : RawContext := sorry
+def RawContext.removeNote (ctx : RawContext) : RawContext :=
+  let newTrack := ctx.track.modifyForgettingFuture fun t =>
+    t.removeNoteAt ctx.cursor.cur.b.val
+  { ctx with track := newTrack }
+
+def RawContext.addNote (ctx : RawContext) : RawContext :=
+  let pitch := ctx.lastPlacedPitch
+  let start := ctx.cursor.cur.b.val
+  let nsteps := 1
+  let newNote : Note := {
+      pitch := pitch,
+      start := start,
+      nsteps := nsteps, hnsteps := by decide
+    }
+  let newTrack := ctx.track.modifyForgettingFuture fun t =>
+    t.removeNoteAt start |>.addNote newNote
+  { ctx with track := newTrack }
+
 def RawContext.moveNoteUpSemitone (ctx : RawContext) : RawContext := sorry
+
 def RawContext.moveNoteDownSemitone (ctx : RawContext) : RawContext := sorry
 
-def RawContext.undoAction (ctx : RawContext) : RawContext := sorry
-def RawContext.redoAction (ctx : RawContext) : RawContext := sorry
+def RawContext.undoAction (ctx : RawContext) : RawContext :=
+  { ctx with track := ctx.track.prev }
 
-def RawContext.undoMovement (ctx : RawContext) : RawContext := sorry
-def RawContext.redoMovement (ctx : RawContext) : RawContext := sorry
+def RawContext.redoAction (ctx : RawContext) : RawContext :=
+  { ctx with track := ctx.track.next }
+
+def RawContext.undoMovement (ctx : RawContext) : RawContext :=
+  { ctx with cursor := ctx.cursor.prev }
+
+def RawContext.redoMovement (ctx : RawContext) : RawContext :=
+  { ctx with cursor := ctx.cursor.next }
 
 end ffi
